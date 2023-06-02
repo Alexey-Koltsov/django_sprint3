67e67fec72f5507bfcd1c29f3c8510ffb9233fda
Алексей Кольцов <koltsov2022@yandex.ru>
2023-06-03 00:18:58 +0300

    Add again_2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -10,9 +10,9 @@ from django.utils import timezone
 def index(request):
     template = 'blog/index.html'
     posts = Post.objects.select_related('category', 'location').filter(
-        Q(pub_date__lte=timezone.now()) &
-        Q(is_published=True) &
-        Q(category__is_published=True)
+        Q(pub_date__lte=timezone.now())
+        & Q(is_published=True)
+        & Q(category__is_published=True)
     ).order_by('-pub_date')[:5]
     context = {'post_list': posts}
     return render(request, template, context)
@@ -22,9 +22,9 @@ def post_detail(request, id):
     template = 'blog/detail.html'
     posts = get_object_or_404(
         Post.objects.select_related('category', 'location').filter(
-            Q(pub_date__lte=timezone.now()) &
-            Q(is_published=True) &
-            Q(category__is_published=True)
+            Q(pub_date__lte=timezone.now())
+            & Q(is_published=True)
+            & Q(category__is_published=True)
         ),
         pk=id
     )
@@ -35,17 +35,14 @@ def post_detail(request, id):
 def category_posts(request, category_slug):
     template = 'blog/category.html'
     categories = get_object_or_404(
-        # Первый аргумент - QuerySet:
         Category.objects.values('title', 'description'),
-        # Второй аргумент - условие,
-        # по которому фильтруются записи из QuerySet:
         slug=category_slug
     )
     posts = get_list_or_404(
         Post.objects.select_related('category').filter(
-            Q(category__slug=category_slug) &
-            Q(is_published=True) &
-            Q(pub_date__lte=timezone.now())
+            Q(category__slug=category_slug)
+            & Q(is_published=True)
+            & Q(pub_date__lte=timezone.now())
         ).order_by('-pub_date'),
         category__is_published=True
     )
