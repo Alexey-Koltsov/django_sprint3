f46b69acbac5d753bb941911b156d2b009525ee6
Алексей Кольцов <koltsov2022@yandex.ru>
2023-06-05 02:42:50 +0300

    Add corrections_1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,48 +1,49 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-
 from blog.models import Category, Post
-
-from django.db.models import Q
-
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.utils import timezone
 
 
 def index(request):
-    template = 'blog/index.html'
     posts = Post.objects.select_related('category', 'location').filter(
-        Q(pub_date__lte=timezone.now())
-        & Q(is_published=True)
-        & Q(category__is_published=True)
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True,
     ).order_by('-pub_date')[:5]
     context = {'post_list': posts}
-    return render(request, template, context)
+    return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id):
-    template = 'blog/detail.html'
     posts = get_object_or_404(
         Post.objects.select_related('category', 'location').filter(
-            Q(pub_date__lte=timezone.now())
-            & Q(is_published=True)
-            & Q(category__is_published=True)
+            pub_date__lte=timezone.now(),
+            is_published=True,
+            category__is_published=True,
         ),
         pk=id
     )
     context = {'post': posts}
-    return render(request, template, context)
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
     categories = get_object_or_404(
         Category.objects.values('title', 'description'),
         slug=category_slug
     )
     posts = get_list_or_404(
-        Post.objects.select_related('category').filter(
-            Q(category__slug=category_slug)
-            & Q(is_published=True)
-            & Q(pub_date__lte=timezone.now())
+        Post.objects.only(
+            'pub_date',
+            'title',
+            'location',
+            'author',
+            'text',
+            'category__slug',
+            'category__title',
+        ).filter(
+            category__slug=category_slug,
+            is_published=True,
+            pub_date__lte=timezone.now(),
         ).order_by('-pub_date'),
         category__is_published=True
     )
@@ -50,4 +51,4 @@ def category_posts(request, category_slug):
         'post_list': posts,
         'category': categories,
     }
-    return render(request, template, context)
+    return render(request, 'blog/category.html', context)
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
@@ -11,5 +11,6 @@ urlpatterns = [
 # Если проект запущен в режиме разработки...
 if settings.DEBUG:
     import debug_toolbar
+
     # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
